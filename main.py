0b26569bc9919ee45b43e4f413d797d551e7ce92
Cameron356 <CManicone@Proton.me>
2024-10-08 18:37:50 -0400

    Chapter:2 Mod:2(Broken)

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 from constants import *
 
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
-print("Starting asteroids!")
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+
+        pygame.Surface.fill(color=(0,0,0))
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
