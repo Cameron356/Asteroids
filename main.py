804a8074a0b600f57e98457cd094510de75032d4
Cameron356 <CManicone@Proton.me>
2024-10-21 18:13:38 -0400

    Chapter:3 Mod:2 | rendered sprits but have not implemented collisions

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
-#from abc import _P
 import pygame
-from pygame.display import update
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #Creating the player
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
-        pygame.Surface.fill(screen, color=(0,0,0))
-        player.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -4,9 +4,13 @@ from constants import *
 from circleshape import CircleShape
 
 class Player(CircleShape):
+    containers = None
+
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
 
     # in the player class
     def triangle(self):
@@ -30,7 +34,7 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a] and not keys[pygame.K_d]:g
+        if keys[pygame.K_a] and not keys[pygame.K_d]:
             self.rotate(-1 * dt)
         if keys[pygame.K_d] and not keys[pygame.K_a]:
             self.rotate(dt)
