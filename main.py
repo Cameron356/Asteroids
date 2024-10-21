e5177f80bdb5468636ebdeabaf8d551de02cbda1
Cameron356 <CManicone@Proton.me>
2024-10-21 14:55:33 -0400

    Chapter:2 Mod:5 | creating and rendering the player

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #Creating the player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color=(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
