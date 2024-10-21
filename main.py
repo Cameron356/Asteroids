21be53d26ec77cb63d15dc14b83fb2d3f08ac97a
Cameron356 <CManicone@Proton.me>
2024-10-21 14:20:11 -0400

    Chapter:2 Mod:4 | added circleshape.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def main():
                 return
         pygame.Surface.fill(screen, color=(0,0,0))
         pygame.display.flip()
-        pygame.time.Clock.tick(40)
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
